5f407a1dff369fc820e0cf63a371c59a9bfd11f9
esonaliyev05 <esonaliyevalyorbek911@gmail.com>
2026-04-13 20:17:21 +0500

    new change

---
+++ app/Features/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function Features() {
+  return (
+    <div className="space-y-5 text-white">
+       <h1 className="text-center  text-[55px]">Hello world</h1>
+
+    </div>
+  );
+}
---
+++ app/Method/page.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Method = () => {
+  
+  return <>
+     <div className="text-center text-[40px] p-[40px]">
+     
+     <h1>Hello word Method</h1>
+
+     </div>
+
+  
+  </>
+
+}
+
+export default Method
